64304d3e57967cae7c0fd446e3a21eceff2884e3
Matt Stauffer <matt@tighten.co>
2018-05-16 13:07:59 -0400

    No Co on Tighten

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,9 +4,9 @@
     @php $description = "Four brains. Two screens. One native app."; @endphp
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <title>Tighten Co. Dev Battle</title>
+    <title>Tighten Dev Battle</title>
     <meta name="description" content="{{ $description }}">
-    <meta name="author" content="Tighten Co.">
+    <meta name="author" content="Tighten">
 
     <meta name="viewport" content="width=device-width">
 
@@ -15,10 +15,10 @@
     <link href="https://fonts.googleapis.com/css?family=Bitter" rel="stylesheet">
 
     <meta property="og:locale" content="en_US">
-    <meta property="og:title" content="Tighten Co. Dev Battle">
+    <meta property="og:title" content="Tighten Dev Battle">
     <meta property="og:description" content="{{ $description }}">
     <meta property="og:url" content="{{ config('app.url') }}">
-    <meta property="og:site_name" content="Tighten Co.">
+    <meta property="og:site_name" content="Tighten">
     <meta property="og:image" content="http://battle.tighten.co/tighten-twitch-dev-battle-og.png">
     <meta property="og:image:width" content="1200">
     <meta property="og:image:height" content="630">
@@ -26,7 +26,7 @@
 <body>
 <div class="wrapper">
     <div class="internal internal--header">
-        <a href="/"><h1><img src="/tighten-twitch-dev-battle-logo.png" alt="Tighten Co. Dev Battle" class="logo"></h1></a>
+        <a href="/"><h1><img src="/tighten-twitch-dev-battle-logo.png" alt="Tighten Dev Battle" class="logo"></h1></a>
 
         <br>
         <a href="/">Battle 2: React Native vs. NativeScript</a> | <a href="/archives/1">Battle 1: Vue vs. React</a>
@@ -37,7 +37,7 @@
     </div>
 
     <div class="internal internal--footer">
-        Put on by the fine folks at <a href="https://tighten.co/">Tighten Co.</a>
+        Put on by the fine folks at <a href="https://tighten.co/">Tighten</a>
     </div>
 </div>
 
