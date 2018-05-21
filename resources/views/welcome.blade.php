e5294c36a2739365514c4c25103184e076b60150
Matt Stauffer <matt@tighten.co>
2018-05-21 11:44:25 -0400

    Fix typo main one

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,25 +1,25 @@
 @extends('layouts.master')
 
 @section('content')
-<h2>Battle 1: React Native vs. NativeScript</h2>
-        <p class="meta">May 25 2018, 11am-3pm Eastern</p>
+<h2>Battle 2: React Native vs. NativeScript</h2>
+<p class="meta">May 25 2018, 11am-3pm Eastern</p>
 
-        <br><br>
-        <div class="iframes">
-            <div class="responsive-iframe">
-                <iframe
-                    src="https://player.twitch.tv/?channel=tightenco"
-                    scrolling="no"
-                    allowfullscreen="true">
-                </iframe>
-            </div>
+<br><br>
+<div class="iframes">
+    <div class="responsive-iframe">
+        <iframe
+            src="https://player.twitch.tv/?channel=tightenco"
+            scrolling="no"
+            allowfullscreen="true">
+        </iframe>
+    </div>
 
-            <div class="responsive-iframe">
-                <iframe
-                    scrolling="yes"
-                    id="tightenco"
-                    src="https://www.twitch.tv/tightenco/chat">
-                </iframe>
-                </div>
+    <div class="responsive-iframe">
+        <iframe
+            scrolling="yes"
+            id="tightenco"
+            src="https://www.twitch.tv/tightenco/chat">
+        </iframe>
         </div>
+</div>
 @endsection
