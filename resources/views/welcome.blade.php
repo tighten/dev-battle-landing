5bc363ce8b6993dc941accfe09393264c84139e4
Matt Stauffer <matt@tighten.co>
2018-05-29 10:11:30 -0400

    Replace Twitch with YouTube for battle 2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
     </h2>
 
     <p class="text-2xl md:text-3xl font-light text-grey-dark pb-6 mb-8">
-        May 25, 2018 &middot; 11<span class="text-lg uppercase"> am</span> - 3<span class="text-lg uppercase"> pm</span> Eastern
+        May 25, 2018 &middot; 11<span class="text-lg uppercase"> am</span> - 3<span class="text-lg uppercase"> pm</span> Eastern (now past)
     </p>
 
     <p class="text-lg md:text-xl font-normal text-grey-dark pb-6 mb-8">
@@ -20,15 +20,19 @@
         </p>
 
         <p class="text-lg md:text-xl font-normal text-grey-dark my-4">
-            Pickr <a href="https://github.com/tightenco/pickr-api">API</a> and <a href="https://github.com/tightenco/pickr-api/tree/master/mockups">app mockups</a>
+            &bull; The underlying API we used for "Pickr": <a href="https://github.com/tightenco/pickr-api">Pickr API</a>
         </p>
 
         <p class="text-lg md:text-xl font-normal text-grey-dark my-4">
-            <a href="https://github.com/tightenco/pickr-reactnative">React Native code</a>
+            &bull; <a href="https://github.com/tightenco/pickr-api/tree/master/mockups">App mockups for Pickr</a>
         </p>
 
         <p class="text-lg md:text-xl font-normal text-grey-dark my-4">
-            <a href="https://github.com/tightenco/pickr-nativescript">NativeScript code</a>
+            &bull; <a href="https://github.com/tightenco/pickr-reactnative">Final React Native code</a>
+        </p>
+
+        <p class="text-lg md:text-xl font-normal text-grey-dark my-4">
+            &bull; <a href="https://github.com/tightenco/pickr-nativescript">Final Vue/NativeScript code</a>
         </p>
     </div>
 
@@ -36,14 +40,22 @@
         <div class="shadow-md p-3 my-8">
             <div class="responsive-iframe">
                 <iframe
+                    src="https://www.youtube-nocookie.com/embed/6zlrr-Xbeo0?rel=0"
+                    frameborder="0"
+                    allow="autoplay; encrypted-media"
+                    allowfullscreen>
+                </iframe>
+                {{--
+                <iframe
                     src="https://player.twitch.tv/?channel=tightenco"
                     scrolling="no"
                     allowfullscreen="true">
                 </iframe>
+                --}}
             </div>
         </div>
 
-        @if (\Carbon\Carbon::now()->lt(\Carbon\Carbon::create(2018, 5, 25, 4, 0, 0, 'America/New_York')))
+        @if (Carbon\Carbon::now()->lt(Carbon\Carbon::create(2018, 5, 25, 4, 0, 0, 'America/New_York')))
             <div class="shadow-md p-3 my-8">
                 <div class="responsive-iframe">
                     <iframe
